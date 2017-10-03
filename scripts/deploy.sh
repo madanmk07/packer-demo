e052422155ee218209c82fc0cdb52428292176e3
madanmk07 <30584127+madanmk07@users.noreply.github.com>
2017-10-03 20:36:29 +0530

    second commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
-wget http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-dpkg -i puppetlabs-release-trusty.deb
 apt-get update
-apt-get install puppet -y
-apt-get install git -y
-	
-#modify config file
-cd /etc/
-rm -fr puppet/
+apt-get install -y nginx nodejs npm
 
-git clone https://github.com/madanmk07/puppet.git
-cd puppet/
+groupadd node-demo
+useradd -d /app -s /bin/false -g node-demo node-demo
 
+mv /tmp/app /app
+chown -R node-demo:node-demo /app
 
-#install modules
-sudo puppet module install puppetlabs-mysql --version 3.10.0
-sudo puppet module install mayflower-php --version 4.0.0-beta1
+echo 'user www-data;
+worker_processes auto;
+pid /run/nginx.pid;
 
-#apply
-puppet apply /etc/puppet/manifests/site.pp
+events {
+        worker_connections 768;
+        # multi_accept on;
+}
+
+http {
+  server {
+    listen 80;
+    location / {
+      proxy_pass http://localhost:3000/;
+      proxy_set_header Host $host;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    }
+  }
+}' > /etc/nginx/nginx.conf
+
+service nginx restart
+
+cd /app
+npm install
+
+echo '[Service]
+ExecStart=/usr/bin/nodejs /app/index.js
+Restart=always
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=node-demo
+User=node-demo
+Group=node-demo
+Environment=NODE_ENV=production
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/node-demo.service
+
+systemctl enable node-demo
+systemctl start node-demo
